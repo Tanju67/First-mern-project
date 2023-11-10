ae11bac1771b0b3abc109f32d08b7527bcfb924a
Tanju Özer <tanjuozer64@gmail.com>
2023-11-10 23:32:29 +0100

    build PlaceItem component

--- src/components/places/Places.js
+++ src/components/places/Places.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Card from "../../shared/UiElements/Card";
+import data from "../../data/data";
+import PlaceItem from "./PlaceItem";
+import classes from "./Places.module.css";
 
 function Places() {
-  return <div>Places</div>;
+  return (
+    <div className={classes.places}>
+      {data.map((d) => (
+        <PlaceItem
+          title={d.title}
+          description={d.description}
+          image={d.image}
+          userImg={d.creatorImg}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default Places;
